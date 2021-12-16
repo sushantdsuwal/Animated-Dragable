eb22f969fcb9f506c0eafb1a12506ce2678a2d15
Sushant Suwal <sushantdsuwal@gmail.com>
2021-12-16 16:38:34 +0545

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useCallback, useState } from 'react';
+import { StyleSheet, View, Text, LayoutChangeEvent } from 'react-native';
+
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import Draggable from './Draggable';
+const SIZE = 100.0;
+const CIRCLE_RADIUS = SIZE * 2;
+
+type ContextType = {
+  translateX: number;
+  translateY: number;
+};
 
 export default function App() {
+  const [layout, setLayout] = useState({
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0,
+  });
+
+  const find_dimensions = useCallback((event: LayoutChangeEvent) => {
+    const { x, y, width, height } = event.nativeEvent.layout;
+    setLayout({ width, height, x, y });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View onLayout={find_dimensions} style={styles.container}>
+        <Draggable xPosition={10} yPosition={10} layout={layout}>
+          <Text>1</Text>
+        </Draggable>
+        <Draggable xPosition={20} yPosition={20} layout={layout}>
+          <Text>1</Text>
+        </Draggable>
+      </View>
+      <View>
+        <Text>height:{layout.height}</Text>
+        <Text>width:{layout.width}</Text>
+        <Text>x:{layout.x}</Text>
+        <Text>y:{layout.y}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container: {
+    margin: 50,
+    height: 700,
+    width: 500,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: 'rgba(0, 0, 256, 0.5)',
+    borderRadius: 20,
+  },
+  circle: {
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: 'rgba(0, 0, 256, 0.5)',
   },
 });
---
+++ Draggable.tsx
@@ -0,0 +1,110 @@
+import 'react-native-gesture-handler';
+import React, { useCallback, useState } from 'react';
+import { Dimensions, StyleSheet, View, Text, LayoutChangeEvent } from 'react-native';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+
+const SIZE = 100.0;
+const CIRCLE_RADIUS = SIZE * 2;
+
+type ContextType = {
+  translateX: number;
+  translateY: number;
+};
+
+interface DraggableProps {
+  children: React.ReactElement;
+  xPosition: number;
+  yPosition: number;
+  layout: {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+  };
+}
+
+export default function Draggable({ children, xPosition = 0, yPosition = 0, layout }: DraggableProps) {
+  const translateX = useSharedValue(xPosition);
+  const translateY = useSharedValue(yPosition);
+
+  const panGestureEvent = useAnimatedGestureHandler<PanGestureHandlerGestureEvent, ContextType>({
+    onStart: (_event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd: () => {
+      const { height, width, x, y } = layout;
+      const xStartPosition = x - SIZE;
+      const xEndPosition = x + width - SIZE;
+      const yStartPosition = y - SIZE;
+      const yEndPosition = y + height - SIZE;
+
+      if (
+        translateX.value > xStartPosition &&
+        translateX.value < xEndPosition &&
+        translateY.value > yStartPosition &&
+        translateY.value < yEndPosition
+      ) {
+        //  do nothing
+      } else {
+        translateX.value = withSpring(xPosition);
+        translateY.value = withSpring(yPosition);
+      }
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
+  return (
+    <PanGestureHandler onGestureEvent={panGestureEvent}>
+      <Animated.View style={[styles.square, rStyle]}>{children}</Animated.View>
+    </PanGestureHandler>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 50,
+    height: 700,
+    width: 500,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: 'rgba(0, 0, 256, 0.5)',
+    borderRadius: 20,
+  },
+  circle: {
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: 'rgba(0, 0, 256, 0.5)',
+  },
+});
